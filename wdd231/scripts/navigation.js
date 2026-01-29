0a95a3084b9851e6a54383b552553a1ea11fe523
akenaivan12 <akenaivan004@gmail.com>
2026-01-29 10:02:36 +0300

    W01 homepage

--- wdd231/scripts/navigation.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.querySelector("#menu");
-const nav = document.querySelector(".navigation");
-
-menuButton.addEventListener("click", () => {
-  nav.classList.toggle("open");
-});
